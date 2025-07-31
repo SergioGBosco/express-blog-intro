d88c33a3307dfd829e5e668a5863456a22d0c356
SergioGBosco <sergiogibosco@gmail.com>
2025-07-31 18:05:31 +0200

    feat: add array  with objcet

--- app.js
+++ app.js
@@ -2,13 +2,42 @@ const express = require(`express`);
 const app = express();
 const port = 3000;
 
-app.get(`/`, (req, res) => {
-  res.send("server del mio blog")
-});
+app.get("/", (req, res) => {
+  const post = [{
+    title: `estate`,
+    content: `spiaggia`,
+    img: `http......`,
+    tags: ["sole", "divertimento", "mare"]
+  },
+  {
+    title: "primavera",
+    content: `al fiume`,
+    img: `http:.....`,
+    tags: ["felicita", "divertimento", "amore"]
+  },
+  {
+    title: "autunno",
+    content: `in montagna`,
+    img: `http:.....`,
+    tags: ["serenita", "divertimento", "amore"]
+  },
+  {
+    title: "Inverno",
+    content: `vicino al camino`,
+    img: `http:.....`,
+    tags: ["noia", "freddo", "amore"]
+  },
+  {
+    title: "natale",
+    content: `foto con l'albero`,
+    img: `http:.....`,
+    tags: ["felicita", "divertimento", "amore"]
+  },
 
-// const postlist = [];
 
+  ];
+  res.send(post);
+})
 app.listen(port, () => {
-
   console.log(`Sto per creare il mio blog nella porta ${port}`)
 });
